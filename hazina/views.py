bb33d736ee8de431b825e3aa3d18c45708d98b00
DeoLwena <deolwena@github.com>
2026-01-16 16:44:43 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	accounts/templates/login.html
    #	accounts/templates/register.html

--- hazina/views.py
+++ hazina/views.py
@@ -1,16 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Mtoaji
 from django.http import HttpResponse
 # Create your views here.
-
+@login_required
 def Addofferings(request):
     return render(request,'add-offering.html')
-
+@login_required()
 def Dashboard(request):
     return render(request,'dashboard.html')
-
+@login_required()
 def Clerks(request):
     return render(request,'Clerks.html')
-
+@login_required()
 def Reports(request):
     return render(request,'Reports.html')
